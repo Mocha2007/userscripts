baeabddba9ca8ff18f46266f803fb6b6ac742bb4
mocha <3129210+Mocha2007@users.noreply.github.com>
2022-07-09 03:07:48 -0400

    fixed ascension

--- cookie clicker/autoEndlessCycle.js
+++ cookie clicker/autoEndlessCycle.js
@@ -1,30 +1,23 @@
 // auto-1k-ascend
 // https://cookieclicker.fandom.com/wiki/Endless_cycle#Strategy
 
-// if game normal
-if (!Game.mocha)
-	// ready to ascend
-	if (1 <= Game.ascendMeterLevel){
-		Game.Ascend(1);
-		Game.mocha = true;
-	}
-	// not ready yet
-	else {
-		// step 1
-		if (Game.Objects.Cursor.amount < 100)
-			Game.Objects.Cursor.buy(100);
-		else if (Game.Objects.Grandma.amount < 100)
-			Game.Objects.Grandma.buy(100);
-		// step 3
-		else {
-			Game.Objects["Fractal engine"].buy(100);
-			Game.Objects.Idleverse.buy(100);
-		}
-		// step 2/4
-		Game.storeBuyAll();
-	}
-// if game in ascend mode
+// ready to ascend
+if (1 <= Game.ascendMeterLevel){
+	Game.Ascend(1);
+	setTimeout(() => Game.Reincarnate(1), 6000); // needs about 6s to load
+}
+// not ready yet
 else {
-	Game.Reincarnate(1);
-	Game.mocha = false;
+	// step 1
+	if (Game.Objects.Cursor.amount < 100)
+		Game.Objects.Cursor.buy(100);
+	else if (Game.Objects.Grandma.amount < 100)
+		Game.Objects.Grandma.buy(100);
+	// step 3
+	else {
+		Game.Objects["Fractal engine"].buy(100);
+		Game.Objects.Idleverse.buy(100);
+	}
+	// step 2/4
+	Game.storeBuyAll();
 }

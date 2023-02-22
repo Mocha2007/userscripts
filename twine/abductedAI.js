1648fab9d39882a5ef5cd2790fd8a61f89555f93
mocha <3129210+Mocha2007@users.noreply.github.com>
2023-02-22 10:10:26 -0500

    Update abductedAI.js

--- twine/abductedAI.js
+++ twine/abductedAI.js
@@ -1,7 +1,7 @@
 // Abducted
 function ai(){
 	const t = 200; // ms; need time for the game to process clicks
-	// let eatenToday = needNewChair = needNewDoor = learnedAll = conjured = adjourned = false;
+	let eatenToday = weighedToday = false;
 	function linkExists(s){
 		return Array.from(document.getElementsByClassName('link-internal'))
 			.find(e => e.innerHTML.includes(s));
@@ -16,9 +16,49 @@ function ai(){
 	function mainLoop(){
 		let elem;
 		// main gameplay
-		if (elem = onlyOption())
+		if (elem = onlyOption()){
+			if (elem.innerHTML === 'Continue'){
+				// name selector
+				const nameElem = document.getElementById('textbox-name');
+				if (nameElem)
+					nameElem.value = 'Luna';
+			}
 			elem.click();
-		// misc
+		}
+		// Bathroom
+		else if (!weighedToday && (elem = linkExists('Enter Bathroom'))
+			elem.click();
+		else if (!weighedToday && (elem = linkExists('Step onto scale')){
+			elem.click();
+			weighedToday = true;
+		}
+		else if (elem = linkExists('Leave Bathroom')
+			elem.click();
+		// Kitchen
+		else if (!eatenToday && (elem = linkExists('Check Stasis Fridge'))
+			elem.click();
+		else if (elem = linkExists('Eat Pastry') // 15 cal/full
+			elem.click();
+		else if (elem = linkExists('Close Stasis Fridge'){
+			elem.click();
+			eatenToday = true;
+		}
+		// Bedroom if no other options available today
+		else if (elem = linkExists('Go to bed'){
+			elem.click();
+			eatenToday = weighedToday = false;
+		}
+		// new game
+		else if (elem = linkExists('Female'))
+			elem.click();
+		else if (elem = linkExists('Next you will choose a name'))
+			elem.click();
+		else if (elem = linkExists('Skip the Prologue'))
+			elem.click();
+		else if (elem = linkExists('Begin'))
+			elem.click();
+		else if (elem = linkExists('What do you want from me?'))
+			elem.click();
 		// unknown event
 		else {
 			clearInterval(interval);

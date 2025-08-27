f82b74a89d50d81570fad51f9bf02779b13ab56e
mocha <3129210+Mocha2007@users.noreply.github.com>
2025-08-27 12:34:59 +0200

    Update sdaah.js

--- social democracy an alternate history/sdaah.js
+++ social democracy an alternate history/sdaah.js
@@ -1,11 +1,11 @@
 // Functions for Social Democracy: An Alternate History
 // https://red-autumn.itch.io/social-democracy
 
-function make_the_nazis_commies(){
-	["workers", "new_middle", "old_middle", "rural", "unemployed", "catholics"].forEach(s => {
+function make_the_nazis_commies(p = 1.0){
+	dendryUI.dendryEngine.state.qualities.classes.forEach(s => {
 		let kpd = s + "_kpd";
 		let nsdap = s + "_nsdap";
-		dendryUI.dendryEngine.state.qualities[kpd] += dendryUI.dendryEngine.state.qualities[nsdap];
-		dendryUI.dendryEngine.state.qualities[nsdap] = 0;
+		dendryUI.dendryEngine.state.qualities[kpd] += p * dendryUI.dendryEngine.state.qualities[nsdap];
+		dendryUI.dendryEngine.state.qualities[nsdap] *= 1.0 - p;
 	});
 }
